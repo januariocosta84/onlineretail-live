6b3bee888d0ffdfa085a663323a4ff2f2844ddff
Januario <jnrdacosta@yahoo.com>
2021-09-10 08:52:26 +0900

    Update settings file

--- TLoretail/settings.py
+++ TLoretail/settings.py
@@ -136,7 +136,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
