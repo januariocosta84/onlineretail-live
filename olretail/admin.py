7dc71b40019e864822655226046e5a0a7b1b6ce0
Januario <jnrdacosta@yahoo.com>
2021-09-22 16:53:02 +0900

    insert data from local database

--- olretail/admin.py
+++ olretail/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from modeltranslation.admin import TranslationAdmin
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -15,4 +16,6 @@ admin.site.register(Country)
 admin.site.register(City)
 admin.site.register(Seller)
 admin.site.register(Comment)
+
+
 
